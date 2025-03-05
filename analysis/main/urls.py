9ada5a628e39c1fcb1d1e3dd13ef4dc441d15ddd
gitpeco <znm9110@163.com>
2025-03-05 23:49:43 +0800

    实现爬取完整数据的功能，现在的问题是如何将数据传回前端

---
+++ analysis/main/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from main import views  # 导入主应用的视图
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('delete_user/', views.delete_user, name='delete_user'),
+    path('search/', views.search, name='search'),
+    path('', views.login, name='index'),  # 将根路径映射到登录视图
+]
